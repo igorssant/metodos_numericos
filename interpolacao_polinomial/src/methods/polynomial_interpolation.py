2e587391b326862f3c1cd0ec43ef1e7861f3c69c
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2025-05-15 11:26:14 -0300

    feat: finalizado metodo da interpolacao de lagrange

---
+++ interpolacao_polinomial/src/methods/polynomial_interpolation.py
@@ -0,0 +1,23 @@
+from numpy.typing import NDArray
+import numpy as np
+
+def lagrange_interpolation(pointwise_matrix:NDArray, poly_order:int, x0:np.float64) -> NDArray:
+    summ:np.float64 = np.float64(0.0)
+    
+    for k in range(poly_order + 1):
+        prod:np.float64 = np.float64(1.0)
+        i:int = 0
+        
+        while(i < k):
+            prod *= (x0 - pointwise_matrix[0, i]) / (pointwise_matrix[0, k] - pointwise_matrix[0, i])
+            i += 1
+            
+        i = k + 1
+        
+        while(i < poly_order + 1):
+            prod *= (x0 - pointwise_matrix[0, i]) / (pointwise_matrix[0, k] - pointwise_matrix[0, i])
+            i += 1
+            
+        summ += prod * pointwise_matrix[1, k]
+    
+    return summ
