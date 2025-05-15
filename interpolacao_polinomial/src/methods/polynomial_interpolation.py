442576735f1bf7cae026b5fc9a10fb8960a90e32
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2025-05-15 12:22:05 -0300

    feat: implementacao da interpolacao de newton iniciada

--- interpolacao_polinomial/src/methods/polynomial_interpolation.py
+++ interpolacao_polinomial/src/methods/polynomial_interpolation.py
@@ -1,6 +1,10 @@
 from numpy.typing import NDArray
 import numpy as np
 
+def newton_interpolation(pointwise_matrix:NDArray, poly_order:int, x0:np.float64) -> tuple[NDArray, NDArray]:
+    # return (yinter, abs_err)
+    pass
+    
 def lagrange_interpolation(pointwise_matrix:NDArray, poly_order:int, x0:np.float64) -> NDArray:
     summ:np.float64 = np.float64(0.0)
     
