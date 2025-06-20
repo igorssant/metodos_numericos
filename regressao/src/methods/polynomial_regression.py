a2d31ac277a43194ccab5f4de3534fa28e95ceb4
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2025-06-20 20:32:51 -0300

    feat: regressão polinomial corrigida e questa 1 - item d) finalizado

--- regressao/src/methods/polynomial_regression.py
+++ regressao/src/methods/polynomial_regression.py
@@ -1,40 +1,20 @@
 from numpy.typing import NDArray
 import numpy as np
 
-def create_augmented_matrix(x:NDArray, y:NDArray, poly_order:int) -> NDArray:
-    data_size:int = len(x)
+def create_augmented_matrix(x: NDArray, y: NDArray, poly_order: int) -> NDArray:
+    data_size = len(x)
     
     if data_size < poly_order + 1:
-        raise ArithmeticError(f"""
-                               Erro: O número de pontos deve ser maior ou igual a ordem do polinômio + 1.
-                               Quantidade de dados = {data_size}.
-                               Ordem + 1 = {poly_order + 1}.
-                               Regressão impossível.
-                               """)
-    
-    augmented_matrix :NDArray = np.zeros((poly_order + 1,
-                                          poly_order + 2),
-                                         dtype=np.float64)
+        raise ArithmeticError("Erro: O número de pontos deve ser maior ou igual a ordem do polinômio + 1.")
 
-    for i in range(1 + poly_order):
-        for j in range(1 + i):
-            k :int = i + j
-            summ :np.float64 = np.float64(0.0)
+    augmented_matrix = np.zeros((poly_order + 1, poly_order + 2), dtype=np.float64)
 
-            for l in range(j):
-                summ += x[l]**2
+    for i in range(poly_order + 1):
+        for j in range(poly_order + 1):
+            augmented_matrix[i, j] = np.sum(x**(i + j))
+            
+        augmented_matrix[i, poly_order + 1] = np.sum(y * (x**i))
 
-            for l in range(j + 1, data_size):
-                summ += x[l]**2
-
-            augmented_matrix[i, j] = summ
-            summ = np.float64(0.0)
-
-        for j in range(data_size):
-            summ += y[j] * x[j]**i
-        
-        augmented_matrix[i, poly_order + 1] = summ
-        
     return augmented_matrix
 
 def retrieve_poly_coef(A: NDArray, b:NDArray) -> NDArray:
