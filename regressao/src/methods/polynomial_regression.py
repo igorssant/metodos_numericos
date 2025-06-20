990358283fbc99b8ea414329b873e14c09535eeb
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2025-06-20 20:48:12 -0300

    feat: iniciado a questao 2

--- regressao/src/methods/polynomial_regression.py
+++ regressao/src/methods/polynomial_regression.py
@@ -1,7 +1,7 @@
 from numpy.typing import NDArray
 import numpy as np
 
-def create_augmented_matrix(x: NDArray, y: NDArray, poly_order: int) -> NDArray:
+def create_augmented_matrix_optimized(x: NDArray, y: NDArray, poly_order: int) -> NDArray:
     data_size = len(x)
     
     if data_size < poly_order + 1:
@@ -10,9 +10,13 @@ def create_augmented_matrix(x: NDArray, y: NDArray, poly_order: int) -> NDArray:
     augmented_matrix = np.zeros((poly_order + 1, poly_order + 2), dtype=np.float64)
 
     for i in range(poly_order + 1):
-        for j in range(poly_order + 1):
-            augmented_matrix[i, j] = np.sum(x**(i + j))
-            
+        for j in range(i + 1): 
+            sum_x_powers = np.sum(x**(i + j))
+            augmented_matrix[i, j] = sum_x_powers
+
+            if i != j:
+                augmented_matrix[j, i] = sum_x_powers
+
         augmented_matrix[i, poly_order + 1] = np.sum(y * (x**i))
 
     return augmented_matrix
